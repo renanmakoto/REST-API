369adb3cff37afe50ac823c2b0c8c6707e19c0ee
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2022-12-15 06:26:08 -0300

    Add files via upload
    A connection to the MongoDB Atlas cloud service was made using Mongoose.

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const userRouter = require('./routes/userRouter')
+const mongoose = require('mongoose')
 
-app.use('/user', userRouter)
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+
+}, (error) => {
+    if (error)
+        console.log(error)
+    else
+        console.log('MongoDB Connected')
+})
+
+app.use('/user', express.json(), userRouter)
 
 app.listen(process.env.PORT, () => {
     console.log("Server running on port 3000")
